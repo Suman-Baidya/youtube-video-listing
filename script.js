d6186945ca84abbbf55a53c2d05245bdd3a6ba50
Suman Baidya <suman.baidya.pro@gmail.com>
2025-03-20 19:07:49 +0530

    modify js file

--- script.js
+++ script.js
@@ -6,35 +6,29 @@ async function getData() {
     return await promise.json();
 }
 
-
 (async () => {
     const result = await getData();
-   
-    console.log(result.data.data[0].items.snippet.thumbnails.default);
-    console.log(result.data.data[0].items.snippet.title);
-    console.log(result.data.data[0].items.snippet.channelTitle);
-    
-    console.log(result.data.data[0].items.snippet.channelTitle);
 
-
+    //Check all API Data
+    // console.log(result.data.data[0].items.snippet.thumbnails.default);
+    // console.log(result.data.data[0].items.snippet.title);
+    // console.log(result.data.data[0].items.snippet.channelTitle);
 
     const videos = result.data.data;
 
-
-
     const videoGrid = document.getElementById("videoGrid");
-        
-        videos.forEach(video => {
-            const videoCard = document.createElement("div");
-            videoCard.classList.add("video-card");
-            videoCard.innerHTML = `
+
+    videos.forEach(video => {
+        const videoCard = document.createElement("div");
+        videoCard.classList.add("video-card");
+        videoCard.innerHTML = `
                 <img src="${video.items.snippet.thumbnails.standard.url}" alt="${video.items.snippet.title}">
                 <div class="video-info">
                     <div class="video-title">${video.items.snippet.title}</div>
                     <div class="channel-name">${video.items.snippet.channelTitle}</div>
                 </div>
             `;
-            videoGrid.appendChild(videoCard);
-        });
-    
+        videoGrid.appendChild(videoCard);
+    });
+
 })();
